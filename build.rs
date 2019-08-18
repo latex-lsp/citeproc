695a6205216ee15a4f1f899c03286ce9bddbe993
Patrick Förster <pfoerster@users.noreply.github.com>
2019-08-18 14:54:35 +0200

    Improve performance

--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("node")
-        .arg("js/build.js")
-        .status()
-        .unwrap();
+    Command::new("node").arg("js/build.js").status().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,36 @@
 use ducc::{Ducc, ExecSettings, Function};
+use once_cell::sync::Lazy;
+use std::sync::Mutex;
 
 const JS: &str = include_str!("../js/dist/citeproc.js");
 
+const WRAPPER: &str = "(function execute(code) { return citeproc.default(code); })";
+
+struct JavaScriptEngine {
+    ducc: Ducc,
+}
+
+impl JavaScriptEngine {
+    pub fn new() -> Self {
+        let ducc = Ducc::new();
+        let _: () = ducc.exec(JS, None, ExecSettings::default()).unwrap();
+        Self { ducc }
+    }
+}
+
+unsafe impl Send for JavaScriptEngine {}
+
+static ENGINE: Lazy<Mutex<JavaScriptEngine>> = Lazy::new(|| Mutex::new(JavaScriptEngine::new()));
+
 pub fn render(code: &str) -> Option<String> {
-    let ducc = Ducc::new();
-    let _: () = ducc.exec(JS, None, ExecSettings::default()).unwrap();
-    let js = "(function execute(code) { return citeproc.default(code); })";
-    let func: Function = ducc.compile(js, None).unwrap().call(()).unwrap();
-    let code = ducc.create_string(&code).unwrap();
+    let engine = ENGINE.lock().unwrap();
+    let code = engine.ducc.create_string(&code).unwrap();
+    let func: Function = engine
+        .ducc
+        .compile(WRAPPER, None)
+        .unwrap()
+        .call(())
+        .unwrap();
     let html: String = func.call((code, 0)).ok()?;
     let markdown = html2md::parse_html(&html);
     Some(markdown.trim().to_string())
@@ -19,7 +42,8 @@ mod tests {
 
     #[test]
     fn success() {
-        let citation = render("@article{foo, author = {Foo Bar}, title = {Baz Qux}, year = 1337}").unwrap();
+        let citation =
+            render("@article{foo, author = {Foo Bar}, title = {Baz Qux}, year = 1337}").unwrap();
         assert_eq!(citation, "Bar, F. (1337). Baz Qux.");
     }
 
